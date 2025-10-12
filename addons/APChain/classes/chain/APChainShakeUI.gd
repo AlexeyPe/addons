7a7e9156f1a413c595512e0312dbcb5de13506ad
Alexey Pechenckin <pechenckinalexey@gmail.com>
2025-10-12 21:37:43 +0500

    CardPack

--- addons/APChain/classes/chain/APChainShakeUI.gd
+++ addons/APChain/classes/chain/APChainShakeUI.gd
@@ -3,9 +3,30 @@ extends APChain
 class_name APChainShakeUI
 
 @export var target:Control
+@export var duration_to:float = 0
+@export var duration_out:float = 1.0
+var tween:Tween
 
+func _tween_method(x):
+	target.material.set_shader_parameter("hit_effect", x)
 
 func _execute(...args:Array) -> void:
 	if target:
 		target.material
-		target.material.set_shader_parameter("y_rot", 1)
+		if tween:
+			tween.kill()
+		if duration_to > 0.0:
+			tween = get_tree().create_tween()
+			tween.tween_method(_tween_method, 0.0, 1.0, duration_to)
+			await tween.finished
+		else:
+			_tween_method(1.0)
+		if duration_out > 0.0:
+			if tween:
+				tween.kill()
+			tween = get_tree().create_tween()
+			tween.tween_method(
+				_tween_method,
+				target.material.get_shader_parameter("hit_effect"),
+				0.0, duration_out
+			)
