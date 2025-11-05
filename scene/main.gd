9e68259e6b3ed0314ed6f8676295b78e418e1c0d
Alexey Pechenckin <pechenckinalexey@gmail.com>
2025-11-05 20:17:33 +0500

    Добавление steam api
    Оверлей работает только в сборке

--- scene/main.gd
+++ scene/main.gd
@@ -1,5 +1,22 @@
 extends Node
 
+const SteamAppID = "480"
+
+func _init() -> void:
+	OS.set_environment("SteamAppID", SteamAppID)
+	OS.set_environment("SteamGameID", SteamAppID)
+
+func _ready() -> void:
+	Steam.steamInit()
+	if !Steam.isSteamRunning():
+		print("_ready(), Steam.isSteamRunning() == false, main.gd")
+		return
+	print("_ready(), Steam.isSteamRunning() == true, main.gd")
+	#var id = 
+	var steam_name = Steam.getFriendPersonaName(Steam.getSteamID())
+	print("steam player name: ", steam_name)
+	
+
 func _unhandled_key_input(_event: InputEvent) -> void:
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().quit()
